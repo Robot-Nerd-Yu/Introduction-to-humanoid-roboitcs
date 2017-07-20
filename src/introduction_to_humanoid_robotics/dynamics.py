b1d5ff101fd72f649de768fb26337542a94a50e0
Robot-Nerd-Yu <Robot-Nerd-Yu@users.noreply.github.com>
2017-07-20 23:35:25 +0800

    Add function to calculate rigid body angular momentum.

--- src/introduction_to_humanoid_robotics/dynamics.py
+++ src/introduction_to_humanoid_robotics/dynamics.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python
+import openravepy as orpy
+import numpy as np
+import IPython
+import rospy
+import tf.transformations as tr
+
+def get_body_angular_momentum(link, omega):
+  R = link.GetTransform()[:3, :3]
+  I_local = link.GetLocalInertia()
+  I = np.dot(R, np.dot(I_local, R.T))
+  L = np.dot(I, omega)
+  return L
+
