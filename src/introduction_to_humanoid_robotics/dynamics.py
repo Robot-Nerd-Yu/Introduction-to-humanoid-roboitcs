0896e95c84cd62fca438470150f23fe7e3227a10
Robot-Nerd-Yu <Robot-Nerd-Yu@users.noreply.github.com>
2017-07-25 00:06:37 +0800

    Major changes: - Update angular momentum function - Add functiont to compute skew symmetric matrix for cross product - Add function to compute Rodrigues' formula

--- src/introduction_to_humanoid_robotics/dynamics.py
+++ src/introduction_to_humanoid_robotics/dynamics.py
@@ -6,9 +6,38 @@ import rospy
 import tf.transformations as tr
 
 def get_body_angular_momentum(link, omega):
+  """
+  Given the angular velocity, compute the anglular momentum.
+  """
   R = link.GetTransform()[:3, :3]
   I_local = link.GetLocalInertia()
+  assert R is not None
+  assert I_local is not None
   I = np.dot(R, np.dot(I_local, R.T))
   L = np.dot(I, omega)
-  return L
+  omegad = - np.dot(np.linalg.inv(I), np.cross(omega, L))
+  return L, I, omegad
 
+def cross_mat(omega):
+  """
+  Compute the skew symmetric matrix.
+  """
+  omega = np.asarray(omega)
+  assert omega.shape[0] == 3
+  wx, wy, wz = omega
+  sn = np.array([[0, -wz, wy],
+                 [wz, 0, -wx],
+                 [-wy, wx, 0]])
+  return sn
+
+def rodrigues(r):
+  """
+  Rodrigues' formula.
+  """
+  r = np.asarray(r)
+  assert r.shape[0] == 3
+  theta = tr.vector_norm(r)
+  a = tr.unit_vector(r)
+  sn = cross_mat(a)
+  R = np.eye(3) + np.sin(theta)*sn + (1-np.cos(theta))*np.dot(sn, sn)
+  return R
